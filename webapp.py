ee931f817a2b29ed2c68b14fda85c1d8c2d840c5
Aalakh <alekhdevari@gmail.com>
2019-12-31 14:51:25 +0530

    webapp.py error fixed

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from config import mac,
 import datetime
 import os
 import dateutil.parser
@@ -19,11 +20,11 @@ def index():
     temp_c_in = 32 
     temp_f = str(round(((9.0 / 5.0) * float(temp_c_in) + 32), 1)) + ' F'
     return render_template("index.html", temp=temp_f)
-
+'''
 @app.route("/update/API_key=<api_key>/mac=<mac>/field=<int:field>/data=<data>", methods=['GET'])
 def update(api_key, mac, field, data):
     return render_template("update.html", data=data)
-
+'''
 @app.route("/update/API_key=<api_key>/mac=<mac>/field=<int:field>/data=<data>", methods=['GET'])
 def write_data_point(api_key, mac, field, data):
     if (api_key == API_KEY and mac == MAC_ADDRESS):
